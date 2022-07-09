8d4c51486c402a0a7c6a1b7ac036dda78f244dce
kirikuchi <damonde6r15b@yahoo.co.jp>
2022-07-09 16:40:40 +0900

    commit

--- src/main/java/com/myapp01/pdf/MyPdfView.java
+++ src/main/java/com/myapp01/pdf/MyPdfView.java
@@ -18,27 +18,28 @@ import com.lowagie.text.pdf.PdfWriter;
 
 public class MyPdfView extends AbstractPdfView {
 
-    protected void buildPdfDocument(Map<String, Object> model, Document doc, PdfWriter writer,
-            HttpServletRequest request, HttpServletResponse response) throws Exception {
+	protected void buildPdfDocument(Map<String, Object> model, Document doc, PdfWriter writer, HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
 
-    	String currentPath = this.getClass().getClassLoader().getResource(".").toString();
-    	currentPath = currentPath.replaceAll("file:", "");
-    	currentPath += "com/myapp01/pdf/";
-    	
-      final String fontFile = currentPath + "ipaexg.ttf";
-      BaseFont cnFont = BaseFont.createFont(fontFile, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-      
-        List<String> words = (List<String>) model.get("wordList");
-        for (String word : words) {
-            doc.add(new Paragraph(word, new Font(cnFont, 12, Font.NORMAL)));
-        }
-     
-        Table table = new Table(2, 2);
-        table.addCell(new Paragraph("first cell"));
-        table.addCell(new Paragraph("second cell"));
-        table.addCell(new Paragraph("third cell"));
-        table.addCell(new Paragraph("test"));
-        
-        doc.add(table);
-    }
+		String currentPath = this.getClass().getClassLoader().getResource(".").toString();
+		currentPath = currentPath.replaceAll("file:", "");
+
+//		final String fontFile = currentPath + "com/myapp01/pdf/ipaexg.ttf";
+		final String fontFile = "/app/src/main/java/com/myapp01/pdf/ipaexg.ttf";
+		
+		BaseFont cnFont = BaseFont.createFont(fontFile, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
+
+		List<String> words = (List<String>) model.get("wordList");
+		for (String word : words) {
+			doc.add(new Paragraph(word, new Font(cnFont, 12, Font.NORMAL)));
+		}
+
+		Table table = new Table(2, 2);
+		table.addCell(new Paragraph("first cell"));
+		table.addCell(new Paragraph("second cell"));
+		table.addCell(new Paragraph("third cell"));
+		table.addCell(new Paragraph("test"));
+
+		doc.add(table);
+	}
 }
